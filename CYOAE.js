68f03e186e558dbeef8cb094c36a20e943acde1b
toeger <toeger@anony.mous>
2020-11-15 22:11:04 +0100

    Added some comments to explain what functions do.

--- CYOAE.js
+++ CYOAE.js
@@ -1,17 +1,20 @@
 "use strict";
 console.log("Started");
 
+// plays through a story arc
 async function play_arc(name) {
     const data = await get(`story arcs/${name}/start.txt`);
     document.body.innerHTML = process(data);
 }
 
+// escapes HTML tags
 function escape(/** @type {String} */ string) {
     let element = document.createElement('p');
     element.innerText = string;
     return element.innerHTML;
 }
 
+// downloads a local resource given its path/filename
 async function get(/** @type {String} */ url) {
     const current_url = window.location.toString().replace(/\/[^\/]*$/, `/`);
     const filepath = `${current_url}${url}`;
@@ -27,23 +30,22 @@ async function get(/** @type {String} */ url) {
     }
 }
 
+//executes [] tags
 function process(/** @type {String} */ source) {
     const result = source.replace(/\[\s*([^\]]*)\s*\]/, (_, /** @type {String} */ code) => {
         const [, tag, params] = code.match(/(\S*)\s*(.*)/);
         switch (tag.toLowerCase()) {
             case "img":
-                console.log("img");
             case "image":
-                console.log("image");
                 return `<img src="${params}">`;
             default:
-                console.log("default");
                 throw `Unknown tag "${tag}"`;
         }
     });
     return result;
 }
 
+// script entry point, loading the correct state and displays errors
 async function main() {
     try {
         await play_arc("intro");
